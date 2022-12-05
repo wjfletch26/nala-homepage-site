358f90dbea7e8a2e7e8e2d4538003c821ebed011
Josh Fletcher <wjfletch26@gmail.com>
2022-12-05 16:31:48 -0600

    debugging, responsive grid styles, Hamburger Icon & Transition, Nav Toggle Js, Mobile Menu

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
 
 function navToggle() {
+  console.log(1)
   btn.classList.toggle('open')
   nav.classList.toggle('hidden')
   document.body.classList.toggle('no-scroll')
 }
 
 btn.addEventListener('click', navToggle)
+
+// const btn = document.getElementById('menu-btn')
+
+// function navToggle()  {
+//   console.log(1)
